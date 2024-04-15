cd42c4d95e80030d25670c142d4b96bdb1b0032f
xiao fu <tianxuefeng2010@gmail.com>
2024-04-15 19:54:17 +0800

    Line

--- TeneasyChatSDK_iOS/Classes/LineLib.swift
+++ TeneasyChatSDK_iOS/Classes/LineLib.swift
@@ -16,48 +16,35 @@ public protocol lineLibDelegate : AnyObject{
 }
 
 struct LineLib{
-    var stop = false
-    
-    func getLine(urlString: String){
-        AF.request(urlString).response { response in
-            if !stop {
+  
+    var delegate: lineLibDelegate?
+    func getLine(urlStrings: [String]){
+         var foundLine = false
+        for urlString in urlStrings {
+            if (foundLine){
+                break
+            }
+            
+            AF.request(urlString).response { response in
+
                 switch response.result {
                 case let .success(value):
-                    let contents = String(data: value!, encoding: .utf8)
+                    //let contents = String(data: value, encoding: .utf8)
                     
-                    guard let contents = contents else { return }
-                    
-                    if !contents.contains("getJSONP._JSONP") {
-                        return
+                    if let v = value, v.count > 5{
+                        foundLine = true
+                        delegate?.useTheLine(line: urlString)
                     }
-                    ///截取两个字符之间的字符串
-                    let startRang = contents.range(of: "(")!
-                    let endRang = contents.range(of: ")")!
-                    let configStr = String(contents[startRang.upperBound..<endRang.lowerBound])
-                    //let configModel = JSONDeserializer<BWConfigModel>.deserializeFrom(json: configStr)
-                    // let message = AESCrypt.decrypt(configModel?.config, password: IFinal.yxAesKey)
-                    
-                    /*
-                     do {
-                     data = try Data(contentsOf: file)
-                     } catch {
-                     fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
-                     }
-                     
-                     do {
-                     let decoder = JSONDecoder()
-                     return try decoder.decode(T.self, from: data)
-                     } catch {
-                     fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
-                     }
-                     */
-                    
-                    
-                    //self?.stop = true
+                  
+                    break
                 case let .failure(error):
                     print(error)
-                
+                    
                 }
+            }
+            
+            if !foundLine{
+                delegate?.lineError(error: "没有可用线路")
             }
         }
     }
