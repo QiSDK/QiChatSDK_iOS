fcbd6d86507950e709145ff0a45e69cab0a35a25
xiaofu2024 <7938813+xiaofu2024@users.noreply.github.com>
2024-10-06 16:37:14 +0800

    1.7.0

--- Example/TeneasyChatSDK_iOS/ViewController.swift
+++ Example/TeneasyChatSDK_iOS/ViewController.swift
@@ -221,7 +221,7 @@ class ViewController: UIViewController, teneasySDKDelegate, LineDetectDelegate, 
         let wssUrl = "wss://" + baseUrl + "/v1/gateway/h5?"
         if lib.payloadId == 0{
             print("initSDK 初始化SDK")
-            lib.myinit(userId: 666665, cert: "COYBEAUYASDyASiG2piD9zE.te46qua5ha2r-Caz03Vx2JXH5OLSRRV2GqdYcn9UslwibsxBSP98GhUKSGEI0Z84FRMkp16ZK8eS-y72QVE2AQ", token: "", baseUrl: wssUrl, sign: "9zgd9YUc", custom: "{\"username\":\"xiaoming\"}")
+            lib.myinit(userId: 666661, cert: "COYBEAUYASDyASiG2piD9zE.te46qua5ha2r-Caz03Vx2JXH5OLSRRV2GqdYcn9UslwibsxBSP98GhUKSGEI0Z84FRMkp16ZK8eS-y72QVE2AQ", token: "", baseUrl: wssUrl, sign: "9zgd9YUc", custom: "{\"username\":\"xiaoming\"}", maxSessionMinutes: 3)
             
             lib.callWebsocket()
             lib.delegate = self
